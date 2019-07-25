cbe0c6d4b787a3f8854d7f84fe123f946bc62ead
missgentle <33093616+missgentle@users.noreply.github.com>
2019-07-25 15:53:28 +0800

    Update index.ts

--- WebpackDemo/src/script/index.ts
+++ WebpackDemo/src/script/index.ts
@@ -1,7 +1,14 @@
 import '@/style/index.css';
 import 'jquery';
 
+// WebpackDemo
 $('#colorpicker').on('input', function(e){
 	console.log('colorpicker change:'+(<HTMLInputElement>e.target).value);
 	window.frames[0].postMessage((<HTMLInputElement>e.target).value, '*');
 })
+
+// WebpackDemo2
+// window.addEventListener('message', function (e) {
+// 	console.log(e.data);
+// 	$('#colorpicker').attr('value',e.data);
+// });
